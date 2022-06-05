ea138bb4a76f16ee010d85ab2ee547361ca8018d
Rohit Abhishek <86154835+xii-vi@users.noreply.github.com>
2022-06-06 03:36:17 +0530

    Update: Mock-Backend Data

--- src/backend/db/users.js
+++ src/backend/db/users.js
@@ -9,15 +9,6 @@ import { formatDate } from "../utils/authUtils";
 export const users = [
   {
     _id: uuid(),
-    firstName: "Adarsh",
-    lastName: "Balika",
-    email: "adarshbalika@gmail.com",
-    password: "adarshbalika",
-    createdAt: formatDate(),
-    updatedAt: formatDate(),
-  },
-  {
-    _id: uuid(),
     firstName: "Rohit",
     lastName: "Abhishek",
     email: "rohitabhishek318@gmail.com",
